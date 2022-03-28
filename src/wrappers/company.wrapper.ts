dec0bd8a8503b2770274d8260ce8a43e4feaa7d9
Caio Emidio de Andrade Carnelós <caio.emidio@gmail.com>
2022-03-28 11:10:32 +0100

    Update company.wrapper.ts

--- src/wrappers/company.wrapper.ts
+++ src/wrappers/company.wrapper.ts
@@ -1,25 +1,24 @@
 import { CompanyService } from "../services/Company/company.service";
-
 const express = require('express');
 
 export class CompanyWrapper {
-  public static getCompany(req: any, res: any) {
+  public static getCompany(req: express.Request, res: express.Response) {
     res.send(CompanyService.getCompany(req.params.id));
   }
 
-  public static getCompanies(req: any, res: any) {
+  public static getCompanies(req: express.Request, res: express.Response) {
     res.send(CompanyService.getCompanies());
   }
 
-  public static saveCompany(req: any, res: any) {
+  public static saveCompany(req: express.Request, res: express.Response) {
     res.send(CompanyService.saveCompany(req.body));
   }
 
-  public static updateCompany(req: any, res: any) {
+  public static updateCompany(req: express.Request, res: express.Response) {
     res.send(CompanyService.updateCompany(req.params.id, req.body));
   }
 
-  public static deleteCompany(req: any, res: any) {
+  public static deleteCompany(req: express.Request, res: express.Response) {
     res.send(CompanyService.deleteCompany(req.params.id));
   }
 
